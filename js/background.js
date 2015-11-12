c7e241becc42574ea8068c6a7332a617a5fe91ee
Skyang <hhyuestc@gmail.com>
2015-11-12 19:07:08 +0800

    优化翻译，部分代码重构

--- js/background.js
+++ js/background.js
@@ -1,21 +1,21 @@
 var omnibox = chrome.omnibox;
 omnibox.setDefaultSuggestion({
-	description: "Define in Google Translate"
+    description: "Define in Youdao Translate"
 });
-omnibox.onInputChanged.addListener(function(text, suggest) {
-	suggest([{
-		content: "http://www.baidu.com/s?wd=" + text,
-		description: "Search " + '"' + text + '"' + " in Baidu"
-	}, {
-		content: "https://www.google.com.hk/?gws_rd=ssl#newwindow=1&q=" + text,
-		description: "Search " + '"' + text + '"' + " in Google"
-	}])
+omnibox.onInputChanged.addListener(function (text, suggest) {
+    suggest([{
+        content: "http://www.baidu.com/s?wd=" + text,
+        description: "Search " + '"' + text + '"' + " in Baidu"
+    }, {
+        content: "https://www.google.com.hk/?gws_rd=ssl#newwindow=1&q=" + text,
+        description: "Search " + '"' + text + '"' + " in Google"
+    }])
 });
-omnibox.onInputEntered.addListener(function(text) {
-	if (text.length < 26) {
-		var tranUrl = "http://translate.google.cn/#en/zh-CN/" + text;
-		window.open(tranUrl)
-	} else {
-		window.open(text)
-	}
+omnibox.onInputEntered.addListener(function (text) {
+    if ( text.length < 26 ) {
+        var tranUrl = "http://translate.google.cn/#en/zh-CN/" + text;
+        window.open(tranUrl)
+    } else {
+        window.open(text)
+    }
 });
--- js/content.js
+++ js/content.js
@@ -1,60 +1,71 @@
-function DOMPop(event){
-	if(document.getElementById("translateDiv")){
-		document.body.removeChild(document.getElementById("translateDiv"));
-	}
-	var selection=document.getSelection();
-	var selectedText=selection.toString();
-	if(selectedText.length > 0){
-		var scrollX=document.body.scrollLeft;
-		var scrollY=document.body.scrollTop;
-		var leftPos=event.clientX+scrollX;
-		var topPos=event.clientY+scrollY;
-		var translatedText;
-		var translating="网络错误，请检查后重试...若反复出现此消息，可发送邮件给我反馈：(hhyuestc@gmail.com)";
-		appendTranslatedDiv(translating,leftPos,topPos);
-		var currentProtocol=location.protocol;
-		var translateHash="//translate.google.cn/translate_a/single?client=t&sl=en&tl=zh-CN&hl=zh-CN&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=sw&ie=UTF-8&oe=UTF-8&prev=conf&psl=en&ptl=en&it=sel.63&ssel=0&tsel=0&q=";
-		var url;
-		if(currentProtocol=="http:"||currentProtocol=="https:"){
-			url = currentProtocol+translateHash+ selectedText;
-		}else{
-			url="http:"+translateHash+selectedText;
-		}
-		translateSend(url, function(res) {
-			var firstIndex = res.indexOf('"');
-			var secIndex = res.indexOf('"', firstIndex + 1);
-			translatedText = res.slice(firstIndex + 1, secIndex);
-		});
-		appendTranslatedDiv(translatedText,leftPos,topPos);
-	}else{
-		selection=null;
-		selectedText=null;
-		return event.preventDefault();
-	}
+function DOMPop(event) {
+    if ( document.getElementById("translateDiv") ) {
+        document.body.removeChild(document.getElementById("translateDiv"));
+    }
+    var selection = document.getSelection();
+    var selectedText = selection.toString();
+    if ( selectedText.length > 0 ) {
+        var scrollX = document.body.scrollLeft;
+        var scrollY = document.body.scrollTop;
+        var leftPos = event.clientX + scrollX;
+        var topPos = event.clientY + scrollY;
+        //var translatedText;
+        var apiKey = "802967876";
+        var keyfrom = "FastSearch";
+        var url = location.protocol + "//fanyi.youdao.com/openapi.do?keyfrom=" + keyfrom + "&key=" + apiKey + "&type=data&doctype=json&version=1.1&q=" + selectedText;
+        translateRequest({
+            url: url,
+            success: function (msg) {
+                var explains = msg.basic.explains;
+                var length = explains.length;
+                var result;
+                for (var i = 0; i < length; i++) {
+                    (function (index) {
+                        if ( result ) {
+                            result += "<p>" + explains[index] + "</p>";
+                        } else {
+                            result = "<p>" + explains[index] + "</p>";
+                        }
+                    })(i);
+                }
+                translatedText = result;
+                appendTranslatedDiv(translatedText, leftPos, topPos);
+            },
+            fail: function (msg) {
+                var translating = "网络错误.若反复出现此消息，可发送邮件给我反馈：(hhyuestc@gmail.com).错误信息为:" + msg;
+                appendTranslatedDiv(translating, leftPos, topPos);
+            }
+        });
+    } else {
+        selection = null;
+        selectedText = null;
+        return event.preventDefault();
+    }
 }
-function appendTranslatedDiv(translatedText,leftPos,topPos){
-	if(document.getElementById("translateDiv")){
-		document.body.removeChild(document.getElementById("translateDiv"));
-	}
-	var translatedHTML="<div id='translateTriangle'></div>"+"<span>"+translatedText+"</span>";
-	var translateDiv=document.createElement("div");
-	translateDiv.id="translateDiv";
-	translateDiv.style.src="../css/content.css";
-	translateDiv.style.left=leftPos+"px";
-	var currentTop=parseInt(topPos+17);
-	translateDiv.style.top=currentTop+"px";
-	translateDiv.innerHTML=translatedHTML;
-	document.body.appendChild(translateDiv);
+function appendTranslatedDiv(translatedText, leftPos, topPos) {
+    if ( document.getElementById("translateDiv") ) {
+        document.body.removeChild(document.getElementById("translateDiv"));
+    }
+    var translatedHTML = "<div id='translateTriangle'></div>" + "<span>" + translatedText + "</span>";
+    var translateDiv = document.createElement("div");
+    translateDiv.id = "translateDiv";
+    translateDiv.style.src = "../css/content.css";
+    translateDiv.style.left = leftPos + "px";
+    var currentTop = parseInt(topPos + 17);
+    translateDiv.style.top = currentTop + "px";
+    translateDiv.innerHTML = translatedHTML;
+    document.body.appendChild(translateDiv);
 }
-function translateSend(url, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("get", url, false);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			callback(xhr.responseText)
-		}
-	};
-	xhr.send();
-}
-document.addEventListener('mouseup',DOMPop,false);
-
+var translateRequest = function (options) {
+    $.ajax({
+        method: "GET",
+        url: options.url,
+        success: function (msg) {
+            options.success(msg);
+        },
+        error: function (msg) {
+            options.fail(msg);
+        }
+    })
+};
+document.addEventListener('mouseup', DOMPop, false);

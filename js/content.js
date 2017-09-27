836cd62e9664bb2ef19156c0f372595de2ec6160
skyang <hhyuestc@gmail.com>
2017-09-27 11:05:39 +0800

    fix: fallback scrollTop

--- js/content.js
+++ js/content.js
@@ -8,17 +8,20 @@ function getConfig() {
     }];
     return (Math.random() > 0.5) ? apiInfo[0] : apiInfo[1];
 }
+
 function DOMPop(event) {
     var selection, selectedText;
     selection = document.getSelection();
     selectedText = selection.toString();
-    if ( document.getElementById("translateDiv") ) {
+    if (document.getElementById("translateDiv")) {
         document.body.removeChild(document.getElementById("translateDiv"));
         selectedText = "";
     }
-    if ( selectedText.length > 0 ) {
-        var scrollX = document.body.scrollLeft;
-        var scrollY = document.body.scrollTop;
+    if (selectedText.length > 0) {
+        // update: chrome v61 doesn't support document.body.scrollTop anymore
+        var scollEle = document.scrollingElement ? document.scrollingElement : document.body
+        var scrollX = scollEle.scrollLeft;
+        var scrollY = scollEle.scrollTop;
         var leftPos = event.clientX + scrollX;
         var topPos = event.clientY + scrollY;
         //var translatedText;
@@ -26,15 +29,15 @@ function DOMPop(event) {
         var url = location.protocol + "//fanyi.youdao.com/openapi.do?keyfrom=" + apiInfo.keyfrom + "&key=" + apiInfo.apiKey + "&type=data&doctype=json&version=1.1&q=" + selectedText;
         translateRequest({
             url: url,
-            success: function (msg) {
-                if ( msg.errorCode == 0 ) {
+            success: function(msg) {
+                if (msg.errorCode == 0) {
                     var translatedText = processMsg(msg);
                     appendTranslatedDiv(translatedText, leftPos, topPos);
                 }
             },
-            fail: function (msg) {
+            fail: function(msg) {
                 var translating = "网络错误.若反复出现此消息，可发送邮件给我反馈：(hhyuestc@gmail.com).错误信息为:" +
-                (typeof(msg) == "object") ? JSON.stringify(msg) : msg;
+                    (typeof(msg) == "object") ? JSON.stringify(msg) : msg;
                 appendTranslatedDiv(translating, leftPos, topPos);
             }
         });
@@ -43,18 +46,19 @@ function DOMPop(event) {
         selectedText = null;
     }
 }
+
 function processMsg(msg) {
     var explains = msg.basic ? msg.basic.explains : msg.translation;
     var phonetic = msg.basic ? msg.basic.phonetic : null;
     var length = explains.length;
     var result;
     //增加音标
-    if ( phonetic ) {
+    if (phonetic) {
         result = "<p>" + "\/" + phonetic + "\/" + "</p>";
     }
     for (var i = 0; i < length; i++) {
-        (function (index) {
-            if ( result ) {
+        (function(index) {
+            if (result) {
                 result += "<p>" + explains[index] + "</p>";
             } else {
                 result = "<p>" + explains[index] + "</p>";
@@ -63,6 +67,7 @@ function processMsg(msg) {
     }
     return result;
 }
+
 function appendTranslatedDiv(translatedText, leftPos, topPos) {
     var translatedHTML = "<div id='translateTriangle'></div>" + "<div>" + translatedText + "</div>";
     var translateDiv = document.createElement("div");
@@ -74,14 +79,15 @@ function appendTranslatedDiv(translatedText, leftPos, topPos) {
     translateDiv.innerHTML = translatedHTML;
     document.body.appendChild(translateDiv);
 }
+
 function translateRequest(options) {
     $.ajax({
         method: "GET",
         url: options.url,
-        success: function (msg) {
+        success: function(msg) {
             options.success(msg);
         },
-        error: function (msg) {
+        error: function(msg) {
             options.fail(msg);
         }
     })
